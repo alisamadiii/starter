807d701c683928c73ed702576ecaa975504db6ff
Ali Samadi <a@alisamadii.com>
2025-10-28 16:43:02 -0700

    react-query + next-theme providers

---
+++ providers/index.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { NextThemeProviders } from "./next-theme";
+import { QueryClientProviders } from "./react-query";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Suspense>
+      <QueryClientProviders>
+          <NextThemeProviders>
+            {children} 
+          </NextThemeProviders>
+        <ReactQueryDevtools />
+      </QueryClientProviders>
+    </Suspense>
+  );
+};
