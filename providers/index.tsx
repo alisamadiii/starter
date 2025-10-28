2f165d79d86904fb3868d699583327a67ccfd1e8
Ali Samadi <a@alisamadii.com>
2025-10-28 16:55:45 -0700

    sonner setup

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import { Toaster } from "@/components/ui/sonner";
+
 import { NextThemeProviders } from "./next-theme";
 import { QueryClientProviders } from "./react-query";
 
@@ -8,7 +10,9 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <Suspense>
       <QueryClientProviders>
-        <NextThemeProviders>{children}</NextThemeProviders>
+        <NextThemeProviders>
+          {children} <Toaster />
+        </NextThemeProviders>
         <ReactQueryDevtools />
       </QueryClientProviders>
     </Suspense>
