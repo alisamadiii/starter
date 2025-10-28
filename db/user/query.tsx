53fb02a4f0386412fe1ebd30d5ab8a82ec9b7f6d
Ali Samadi <a@alisamadii.com>
2025-10-28 16:45:55 -0700

    fetching folder structure

---
+++ db/user/query.tsx
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { getUser } from "./action";
+
+export const useGetUser = async () => {
+  return useQuery({
+    queryKey: ["user"],
+    queryFn: getUser,
+  });
+};
